c191138a69573beca5d9878b27317f5b0884c220
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-04-30 19:47:34 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Byeon jinha on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/memoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoAppApp.swift
+//  Shared
+//
+//  Created by Byeon jinha on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct memoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
